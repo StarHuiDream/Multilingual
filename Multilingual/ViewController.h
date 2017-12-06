bcaac26907b334df8088f2016caa796a59d890ee
StarHui <StarHuiDream@yeah.net>
2017-12-06 15:55:10 +0800

    Initial Commit

---
+++ Multilingual/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multilingual
+//
+//  Created by StarHui on 2017/12/6.
+//  Copyright © 2017年 StarHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Multilingual/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Multilingual
+//
+//  Created by StarHui on 2017/12/6.
+//  Copyright © 2017年 StarHui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
